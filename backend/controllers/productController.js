79f100f03aab4e4081fd145a9dd66bee523f6c24
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-02-26 10:22:28 +0530

    Initialized my e commrece project web site repository

---
+++ backend/controllers/productController.js
@@ -0,0 +1,22 @@
+
+const productModel = require('../models/productModel');
+const ProductModel = require('../models/productModel');
+
+
+
+exports.getProducts = async (req, res, next) => {
+
+   const products = await productModel.find({});
+
+    res.json({
+        success: true,
+        products
+    })
+}
+exports.getSingleProduct = (req, res, next) => {
+    res.json({
+        success: true,
+        message: 'Get single product working'
+    })
+}
+
