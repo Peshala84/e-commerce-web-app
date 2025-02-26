780e4cdbcf669786daa27ea27abae1b0d8d884d8
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-02-27 00:39:40 +0530

    Implement order creation logic in orderController

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,10 @@
+const orderModel = require('../models/orderModel');
+
 exports.createOrder = (req, res, next) => {
+    const cartItems = req.body;
+    const amount = cartItems.reduce((acc, items) => (acc + item.product.price * items.qty), 0);
+    console.log(amount, 'AMOUNT');
+    // orderModel.create()
     res.json({
         success: true,
         message: ' Order works!'
